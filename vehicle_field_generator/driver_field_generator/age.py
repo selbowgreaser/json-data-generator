50043ed0b39e5702ccb9c95da3d33ab77f31aef9
selbowgreaser <selbowgreaser@gmail.com>
2022-10-30 12:29:09 +0300

    Replaced uniform distribution by normal

--- vehicle_field_generator/driver_field_generator/age.py
+++ vehicle_field_generator/driver_field_generator/age.py
@@ -4,12 +4,19 @@ import random
 class AgeGenerator:
     def __init__(self,
                  minimum_age: int = 18,
-                 maximum_age: int = 99):
+                 maximum_age: int = 70,
+                 mu: int = 35,
+                 sigma: int = 12):
         self.minimum_age = minimum_age
         self.maximum_age = maximum_age
+        self.mu = mu
+        self.sigma = sigma
 
     def generate(self) -> int:
-        return random.randint(self.minimum_age, self.maximum_age)
+        age = round(random.normalvariate(self.mu, self.sigma))
+        while not self.minimum_age <= age <= self.maximum_age:
+            age = round(random.normalvariate(self.mu, self.sigma))
+        return age
 
 
 if __name__ == "__main__":
