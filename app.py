8de29d28622c1d3265eb6e623769f98af0d6da57
selbowgreaser <selbowgreaser@gmail.com>
2022-11-09 01:36:56 +0300

    Added work with kwargs

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-import sys
-
+from args_parser import ArgsParser
 from mq import *
 from generator.common.guid import *
 from generator.vehicle import *
 from generator.vehicle.car_field import *
 from generator.vehicle.driver_field import *
 
-argv = sys.argv
-mq_size = int(sys.argv[1]) if len(argv) > 1 else 999
-time_interval = int(sys.argv[2]) if len(argv) > 2 else 0
-print(f"Получено аргументов из командной строки: {len(argv) - 1}")
-print(f"Будет отправлено {mq_size} сообщений с интервалом {time_interval} секунд")
+kwargs = ArgsParser.get_args()
+
+print(f"Получено {len(kwargs)} аргументов из командной строки: {kwargs}")
 
 if __name__ == "__main__":
     MqCreator(
         KafkaMessageSender(
             KafkaKeyGenerator(
-                GuidGenerator()
-            )
+                GuidGenerator(),
+            ),
+            **kwargs
         ),
         VehicleGenerator(
             GuidGenerator(),
@@ -34,6 +32,5 @@ if __name__ == "__main__":
                 PhoneNumberGenerator()
             )
         ),
-        mq_size=mq_size,
-        time_interval=time_interval
+        **kwargs
     ).run()
