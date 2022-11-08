bebcf2cac57a283de9e77c8ac698f25bb10c4777
selbowgreaser <selbowgreaser@gmail.com>
2022-11-09 01:37:43 +0300

    Configured work with kwargs

--- mq/mq_creator.py
+++ mq/mq_creator.py
@@ -3,18 +3,20 @@ import time
 from generator.vehicle import VehicleGenerator
 from mq import KafkaMessageSender
 
+DEFAULT_MQ_SIZE = 999
+DEFAULT_TIME_INTERVAL = 0
+
 
 class MqCreator:
     def __init__(self,
                  kafka_message_sender: KafkaMessageSender,
                  vehicle_generator: VehicleGenerator,
-                 mq_size: int,
-                 time_interval: int,
+                 **kwargs
                  ):
         self.kafka_message_sender = kafka_message_sender
         self.vehicle_generator = vehicle_generator
-        self.mq_size = mq_size
-        self.time_interval = time_interval
+        self.mq_size = kwargs["mq_size"] if "mq_size" in kwargs else DEFAULT_MQ_SIZE
+        self.time_interval = kwargs["time_interval"] if "time_interval" in kwargs else DEFAULT_TIME_INTERVAL
 
     def run(self):
         for i in range(1, self.mq_size + 1):
