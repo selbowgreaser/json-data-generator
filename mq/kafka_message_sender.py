5127465c54bcc8bf037bc1d711f064912934a7f2
selbowgreaser <selbowgreaser@gmail.com>
2022-11-09 01:37:39 +0300

    Configured work with kwargs

--- mq/kafka_message_sender.py
+++ mq/kafka_message_sender.py
@@ -11,17 +11,16 @@ KAFKA_BROKERS = ('localhost:29092',)
 class KafkaMessageSender:
     def __init__(self,
                  kafka_key_generator: KafkaKeyGenerator,
-                 kafka_topic: str = KAFKA_TOPIC,
-                 kafka_brokers: list = KAFKA_BROKERS,
-                 include_none: bool = False):
+                 **kwargs):
         self.kafka_key_generator = kafka_key_generator
-        self.kafka_topic = kafka_topic
-        self.kafka_brokers = kafka_brokers
-        self.include_none = include_none
+        self.kafka_topic = kwargs["kafka_topic"] if "kafka_topic" in kwargs else KAFKA_TOPIC
+        self.kafka_brokers = kwargs["kafka_brokers"] if "kafka_brokers" in kwargs else KAFKA_BROKERS
+        self.include_none = kwargs["include_none"] if "include_none" in kwargs else False
 
     def send(self, value):
         producer = KafkaProducer(bootstrap_servers=self.kafka_brokers,
-                                 value_serializer=lambda x: bytes(json.dumps(x, default=lambda obj: obj.__getstate__(self.include_none)), "utf-8"),
+                                 value_serializer=lambda x: bytes(
+                                     json.dumps(x, default=lambda obj: obj.__getstate__(self.include_none)), "utf-8"),
                                  key_serializer=lambda x: bytes(x, 'utf-8'),
                                  max_request_size=100001200)
         producer.send(self.kafka_topic, key=self.kafka_key_generator.generate(), value=value).get()
